063c52469f52d8cc2706305fab094a9a9ef37b70
Aditya-130205 <4dity4aryandi46@gmail.com>
2025-05-26 14:50:52 +0800

    modifikasi kode program agar menerima masukan jumlah nama yang akan di proses

--- hello.cpp
+++ hello.cpp
@@ -5,24 +5,31 @@ using namespace std;
 
 int main()
 {
-    string nama[5]; //inisialisasi array string untuk menampung 5 nama
+    int jumlahNama; //inisialisasi array string untuk menampung 5 nama
+    system("cls");
+
+    // Memasukkan input jumlah nama atau jumlah nama yang ingin di masukkan 
+    cout << "Masukkan jumlah nama yang ingin dimasukkan: ";
+    cin >> jumlahNama;
+    cin.ignore();
+
+    string* nama = new string[jumlahNama]; // Deklarasi array dinamis untuk menyimpan nama
 
     // Input memasukan data 5 buah nama  ke dalam array string 
-    system("cls");
-    cout << "Silakan masukkan 5 nama:\n";
-    for (int i = 0; i < 5; i++)
+    cout << "Masukkan " << jumlahNama << " nama:\n";
+    for (int i = 0; i < jumlahNama; i++)
     {
         cout << i + 1 << ". Nama: ";
         getline(cin, nama[i]);
     }
-
-     // Menampilkan output  data pada array 
+    
+    // Menampilkan output  data pada array 
     cout << "\nDaftar Nama yang Telah Dimasukkan:\n";
     cout << "+-------+--------------------------+" << endl;
     cout << "| No    | Nama                     |" << endl;
     cout << "+-------+--------------------------+" << endl;
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < jumlahNama; i++)
     {
         cout << "| " << setw(5) << right << i + 1 << " | "
              << setw(25) << left << nama[i] << "|" << endl;
@@ -31,6 +38,7 @@ int main()
 
     cout << "+-------+--------------------------+" << endl;
 
+    delete[] nama;
 
     return 0;
 } 
